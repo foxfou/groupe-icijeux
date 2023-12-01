abd667ece2f96fead9c99735b57a33ffb8885c77
foxfou <1shibbygames1@gmail.com>
2023-12-01 14:29:20 +0300

    fix: logo path

--- src/components/images/LogoBlack.tsx
+++ src/components/images/LogoBlack.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function LogoBlack() {
     return (
-        <Image src="/groupe-icijeux/logo-black.png" alt="Black logo" height="85" width="298" />
+        <Image src="/groupe-icijeux/logo-black.png" alt="Black logo" height="86" width="298" />
     )
 }
